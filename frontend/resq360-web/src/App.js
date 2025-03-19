d78ede2f8cf2508be6daba637d28b806f07374bc
rahul <workmail.rahulsaini@gmail.com>
2025-03-19 19:23:28 +0530

    second

--- frontend/resq360-web/src/App.js
+++ frontend/resq360-web/src/App.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import EmergencyScreen from './screens/Emergencyscreen';
-import ChatbotScreen from './screens/ChatbotScreen';
-import SettingsScreen from './screens/SettingsScreen';
-import EmergencyNumbers from './components/EmergencyNumbers';
-import VoiceCommand from './components/VoiceCommand';
-import ShakeDetection from './components/ShakeDetection';
-import { sendSOS } from './services/sos';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AboutPage from './components/AboutPage';
+import ContactPage from './components/ContactPage';
 
 function App() {
-  const handleSOS = () => {
-    alert("SOS Triggered! Sending alert...");
-    sendSOS("SOS Activated! Help needed.");
-  };
-
   return (
     <Router>
-      <VoiceCommand onSOS={handleSOS} />
-      <ShakeDetection onSOS={handleSOS} />
-      <Switch>
-        <Route path="/" exact component={HomeScreen} />
-        <Route path="/emergency" component={EmergencyScreen} />
-        <Route path="/chatbot" component={ChatbotScreen} />
-        <Route path="/settings" component={SettingsScreen} />
-        <Route path="/emergency-numbers" component={EmergencyNumbers} />
-      </Switch>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
---
+++ frontend/resq360-web/src/components/ContactPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>This is the contact page of the ResQ360 web application.</p>
+    </div>
+  );
+}
+
+export default ContactPage;
